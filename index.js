409e544c213708fce1624f6ef37eb13d57a89e0a
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-07 17:26:14 -0400

    Add user choice
    Prompt user to enter rock, paper,
     or scissors. If a valid value, print
     value to console.  If invalid value is
     entered, alert an error and initiate
     loop of prompt, if..else if..else
     checks. Users may input invalid
     values, so the program must be
     able to ask for another value
     infinitely, until the user inputs an
     acceptable one.

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ let getComputerChoice = (rockPaperOrScissors) => {
 }
 
 getComputerChoice();
+
+//Get user decision: rock, paper, or scissors. If invalid value is entered alert error and loop prompt and check sequence.
+let userInput;
+let verdict;
+let getUserChoice = (rockPaperOrScissors) => {
+    do {
+        userInput = prompt("Let's Play a Game: Type one of the following in the box below: 'Rock', 'Paper', or 'Scissors'")
+        if (userInput.toLowerCase() === "rock") {
+            console.log("User Choice: 'Rock'");
+            verdict = "pass";
+        } else if (userInput.toLowerCase() === "paper"){
+            console.log("User Choice: 'Paper'");
+            verdict = "pass";
+        } else if (userInput.toLowerCase() === "scissors"){
+            console.log("User Choice: 'Scissors'");
+            verdict = "pass";
+        } else {
+            alert("Error: Please type into the box 'Rock', 'Paper', or 'Scissors'");
+            verdict = "fail";
+        }
+    }
+    while (verdict === "fail");
+}
+
+getUserChoice();

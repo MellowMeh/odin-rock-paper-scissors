56dbdfafaecc2cb3636208214174ae0ccb0c02a8
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-09 13:55:49 -0400

    Fix console display computerChoice before user
    Previously, the computer's random decimal and choice "rock"
    displayed in the console before the user made their decision.
    This meant the user could win every time. Now, the computer
    choice "rock" is logged in the console only after the userInput is collected.

--- index.js
+++ index.js
@@ -7,16 +7,12 @@
 let computerDecision
 let getComputerChoice = (rockPaperOrScissors) => {
     computerDecision = Math.random();
-        console.log(computerDecision);
         if (computerDecision < 0.33) {
             computerDecision = "rock";
-            console.log("Computer Choice: 'Rock'");
         } else if (computerDecision > 0.33 && computerDecision < 0.66) {
             computerDecision = "paper";
-            console.log("Computer Choice: 'Paper'");
         } else {
             computerDecision = "scissors";
-            console.log("Computer Choice: 'Scissors'");
         }
 }
 
@@ -50,28 +46,37 @@ getUserChoice();
 //Play a round - a function takes arguments of the human choice and the computer choice. If...else statements are used to decide victory or defeat alert and which score variable is incremented.
 let playRound = (humanChoice, computerChoice) => {
     if (userInput.toLowerCase() === "rock" && computerDecision === "rock") {
+        console.log("Computer Choice: 'Rock'");
         console.log("It's a draw! Play again?");
     } else if (userInput.toLowerCase() === "rock" && computerDecision === "paper") {
+        console.log("Computer Choice: Paper");
         console.log("Oh no! You lost that round. Play again?");
         ++computerScore;
     } else if (userInput.toLowerCase() === "rock" && computerDecision === "scissors") {
+        console.log("Computer Choice: Scissors");
         console.log("Hooray! You won that round. Play again?");
         ++humanScore;
     } else if (userInput.toLowerCase() === "paper" && computerDecision === "rock") {
+        console.log("Computer Choice: 'Rock'");
         console.log("Hooray! You won that round. Play again?");
         ++humanScore;
     } else if (userInput.toLowerCase() === "paper" && computerDecision === "paper") {
+        console.log("Computer Choice: Paper");
         console.log("It's a draw! Play again?");
     } else if (userInput.toLowerCase() === "paper" && computerDecision === "scissors") {
+        console.log("Computer Choice: Scissors");
         console.log("Oh no! You lost that round. Play again?")
         ++computerScore;
     } else if (userInput.toLowerCase() === "scissors" && computerDecision === "rock") {
+        console.log("Computer Choice: 'Rock'");
         console.log("Oh no! You lost that round. Play again?");
         ++computerScore;
     } else if (userInput.toLowerCase() === "scissors" && computerDecision === "paper") {
+        console.log("Computer Choice: Paper");
         console.log("Hooray! You won that round. Play again?");
         ++humanScore;
     } else if (userInput.toLowerCase() === "scissors" && computerDecision === "scissors") {
+        console.log("Computer Choice: Scissors");
         console.log("It's a draw! Play again?");
     } else {
         console.log("Error: answer not recognized");

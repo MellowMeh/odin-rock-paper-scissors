887681f612b8747110c5778eeb7c194c26bf225e
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-18 17:20:20 -0400

    Fix eventListener to run function

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ let displayScore = () => {
         scoreTextComputer.textContent = ("Computer score: " + computerScore);
     }
 
-container.addEventListener('click', (event) => {
+let playRound = (event) => {
     let target = event.target;
 
     switch(target.id) {
@@ -138,96 +138,11 @@ container.addEventListener('click', (event) => {
             }
             break;
     }
-    userInput = "";
-    computerDecision = "";
-});
+};
 
-//Resolution - when user or computer achieves 5 points, victory or loss message displays and user may choose to play again
+container.addEventListener('click', playRound);
 
-
-
-
-
-/*
-    let playRound = (userInput, computerDecision) => {
-        switch(userInput) {
-            case 'rock':
-                
-                break;
-
-            case 'paper':
-                switch(computerDecision) {
-                    case 'rock':
-                        break;
-                    case 'paper':
-                        break;
-                    case 'scissors':
-                        break;
-                }
-                break;
-
-            case 'scissors':
-                switch(computerDecision) {
-                    case 'rock':
-                        break;
-                    case 'paper':
-                        break;
-                    case 'scissors':
-                        break;
-                }    
-            break;
-        }
-    }
-})
-
-playRound();
-
-//Play a round - a function takes arguments of the human choice and the computer choice. If...else statements are used to decide victory or defeat alert and which score variable is incremented.
-
-
-
-let playRound = (humanChoice, computerChoice) => {
-    if (userInput.toLowerCase() === "rock" && computerDecision === "rock") {
-        console.log("Computer Choice: 'Rock'");
-        console.log("It's a draw! Play again?");
-    } else if (userInput.toLowerCase() === "rock" && computerDecision === "paper") {
-        console.log("Computer Choice: Paper");
-        console.log("Oh no! You lost that round. Play again?");
-        ++computerScore;
-    } else if (userInput.toLowerCase() === "rock" && computerDecision === "scissors") {
-        console.log("Computer Choice: Scissors");
-        console.log("Hooray! You won that round. Play again?");
-        ++humanScore;
-    } else if (userInput.toLowerCase() === "paper" && computerDecision === "rock") {
-        console.log("Computer Choice: 'Rock'");
-        console.log("Hooray! You won that round. Play again?");
-        ++humanScore;
-    } else if (userInput.toLowerCase() === "paper" && computerDecision === "paper") {
-        console.log("Computer Choice: Paper");
-        console.log("It's a draw! Play again?");
-    } else if (userInput.toLowerCase() === "paper" && computerDecision === "scissors") {
-        console.log("Computer Choice: Scissors");
-        console.log("Oh no! You lost that round. Play again?")
-        ++computerScore;
-    } else if (userInput.toLowerCase() === "scissors" && computerDecision === "rock") {
-        console.log("Computer Choice: 'Rock'");
-        console.log("Oh no! You lost that round. Play again?");
-        ++computerScore;
-    } else if (userInput.toLowerCase() === "scissors" && computerDecision === "paper") {
-        console.log("Computer Choice: Paper");
-        console.log("Hooray! You won that round. Play again?");
-        ++humanScore;
-    } else if (userInput.toLowerCase() === "scissors" && computerDecision === "scissors") {
-        console.log("Computer Choice: Scissors");
-        console.log("It's a draw! Play again?");
-    } else {
-        console.log("Error: answer not recognized");
-    }
-    console.log(humanScore, computerScore)
-}
-
-
-//Play five rounds of play. When the user or the computer reaches a score of 5, the game ends, the prompt to insert an answer disappears, and a final message is displayed. 
+/*Resolution - when user or computer achieves 5 points, victory or loss message displays and user may choose to play again
 let playGame = (fiveRoundsOfPlay) => {
     playRound();
     do {
@@ -246,5 +161,5 @@ let playGame = (fiveRoundsOfPlay) => {
 
 
 playGame();
-
 */
+
